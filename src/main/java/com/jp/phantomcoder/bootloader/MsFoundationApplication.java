39d2744d4bc59505943e2c6746b412947eb64f5c
Lokesh Konidala <lokesh@Lokeshs-MBP.elecom>
2022-10-20 22:46:10 +0900

    Initial Commit

---
+++ src/main/java/com/jp/phantomcoder/bootloader/MsFoundationApplication.java
@@ -0,0 +1,13 @@
+package com.jp.phantomcoder.bootloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFoundationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFoundationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/phantomcoder/MsFoundationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.phantomcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFoundationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
